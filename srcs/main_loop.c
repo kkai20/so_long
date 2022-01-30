e9e62da1116de31cd3951febc36e7e02adde6780
kkai10 <kaneko.umi1020@gmail.com>
2022-01-30 17:40:40 +0900

    maybe

--- srcs/main_loop.c
+++ srcs/main_loop.c
@@ -6,7 +6,7 @@
 /*   By: kkai <kkai@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 17:58:32 by kkai              #+#    #+#             */
-/*   Updated: 2022/01/30 17:33:21 by kkai             ###   ########.fr       */
+/*   Updated: 2022/01/30 17:40:11 by kkai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,16 @@ int	main_loop(t_vars *game)
 	int		i;
 	int		j;
 
-	if (game->key_flag == 1)
+	// if (game->key_flag == 1 || 1)
+	if (1)
 	{
 		i = 0;
 		while (i < game->rows)
 		{
 			j = 0;
-			// if (game->map[i][j] == PLAYER)
-			mlx_put_image_to_window(game->mlx, game->win, game->images[PLAYER],
-			TILE_SIZE * game->player_x, TILE_SIZE * game->player_y);
+			if (game->map[i][j] == PLAYER)
+				mlx_put_image_to_window(game->mlx, game->win, game->images[PLAYER],
+				TILE_SIZE * game->player_x, TILE_SIZE * game->player_y);
 			while (j < game->cols)
 			{
 				// printf("%d\n", game->map[i][j]);
