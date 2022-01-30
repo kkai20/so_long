50ac999fc0057378ccbccd9114323486b54a2068
kkai10 <kaneko.umi1020@gmail.com>
2022-01-30 18:41:13 +0900

    false

--- srcs/main_loop.c
+++ srcs/main_loop.c
@@ -6,7 +6,7 @@
 /*   By: kkai <kkai@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 17:58:32 by kkai              #+#    #+#             */
-/*   Updated: 2022/01/30 17:40:11 by kkai             ###   ########.fr       */
+/*   Updated: 2022/01/30 18:40:29 by kkai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,25 +21,28 @@ int	main_loop(t_vars *game)
 	if (1)
 	{
 		i = 0;
+				mlx_put_image_to_window(game->mlx, game->win, game->images[PLAYER],
+				TILE_SIZE * game->player_x, TILE_SIZE * game->player_y);
 		while (i < game->rows)
 		{
 			j = 0;
-			if (game->map[i][j] == PLAYER)
-				mlx_put_image_to_window(game->mlx, game->win, game->images[PLAYER],
-				TILE_SIZE * game->player_x, TILE_SIZE * game->player_y);
 			while (j < game->cols)
 			{
-				// printf("%d\n", game->map[i][j]);
-				mlx_put_image_to_window(game->mlx, game->win,
-					game->images[game->map[i][j]],
-					TILE_SIZE * j, TILE_SIZE * i);
-				j++;
-			}
+				printf("%d\n", game->map[i][j]);
+				if (game->map[i][j] != PLAYER)
+					{
+						// printf("hello\n");
+						mlx_put_image_to_window(game->mlx, game->win,
+							game->images[game->map[i][j]],
+							TILE_SIZE * j, TILE_SIZE * i);
+					}
+					j++;
+				}
 			i++;
 		}
-		// mlx_put_image_to_window(game->mlx, game->win, game->images[PLAYER],
-		// 	TILE_SIZE * game->player_x, TILE_SIZE * game->player_y);
+	// 	mlx_put_image_to_window(game->mlx, game->win, game->images[PLAYER],
+	// 		TILE_SIZE * game->player_x, TILE_SIZE * game->player_y);
 	}
-	game->key_flag = 0;
+	// game->key_flag = 0;
 	return (0);
 }
