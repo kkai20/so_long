8db792f18c2d43b302116fae4e90fe9c7eb4c772
kkai10 <kaneko.umi1020@gmail.com>
2022-01-30 20:44:56 +0900

    f

--- srcs/main_loop.c
+++ srcs/main_loop.c
@@ -5,50 +5,33 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kkai <kkai@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/21 17:58:32 by kkai              #+#    #+#             */
-/*   Updated: 2022/01/30 19:18:18 by kkai             ###   ########.fr       */
+/*   Created: 2022/01/30 20:38:38 by kkai              #+#    #+#             */
+/*   Updated: 2022/01/30 20:44:07 by kkai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	main_loop(t_vars *game)
+int main_loop(t_vars *game)
 {
-	int		i;
-	int		j;
+	int i;
+	int j;
 
-	// if (game->key_flag == 1 || 1)
-	if (1)
+	i = 0;
+	while (i < game->rows)
 	{
-		i = 0;
-
-		while (i < game->rows)
+		j = 0;
+		while (j < game->cols)
 		{
-			j = 0;
-			while (j < game->cols)
-			{
-				//
-				// 0000
-				// 1111
-				// 1P11
-				// 1111 j
-				// i
-
-					mlx_put_image_to_window(game->mlx, game->win, game->images[PLAYER], TILE_SIZE * game->player_x, TILE_SIZE * game->player_y);
-				// if (game->map[i][j] == 3)
-				// 	printf("%d\n", game->map[i][j]);
-						// printf("hello\n");
-						if (game->map[i][j] != 4)
-						mlx_put_image_to_window(game->mlx, game->win,
-							game->images[game->map[i][j]],
-							TILE_SIZE * j, TILE_SIZE * i);
-				j++;
-			}
-			i++;
+			if (i == game->player_y && j == game->player_x)
+				mlx_put_image_to_window(game->mlx, game->win, game->images[PLAYER],
+				TILE_SIZE * game->player_x, TILE_SIZE * game->player_y);
+			else
+				mlx_put_image_to_window(game->mlx, game->win,
+				game->images[game->map[i][j]], TILE_SIZE * j, TILE_SIZE * i);
+			j++;
 		}
-	// 	mlx_put_image_to_window(game->mlx, game->win, game->images[PLAYER],
-	// 		TILE_SIZE * game->player_x, TILE_SIZE * game->player_y);
+		i++;
 	}
-	// game->key_flag = 0;
 	return (0);
 }
